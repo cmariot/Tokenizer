5e480a1c71051a9b8bf8f5257d4c3b78b4a56965
cmariot <contact@charles-mariot.fr>
2025-02-06 00:47:35 +0100

    ERC20 Token

--- code/contracts/AlphaCoin42.sol
+++ code/contracts/AlphaCoin42.sol
@@ -11,7 +11,6 @@ pragma solidity ^0.8.20;
 
 contract AlphaCoin42 {
 
-
     // State variables
 
     // Name of the token
@@ -22,22 +21,32 @@ contract AlphaCoin42 {
     uint8 private _decimals = 18;
     // Total supply of the token
     uint256 private _totalSupply;
-
-
+    // Mapping from token owner address to their balance.
+    mapping(address => uint256) private _balances;
+    // Imbricated mapping from token owner address to spender address to the
+    // amount of tokens they are allowed to transfer.
+    mapping(address => mapping(address => uint256)) private _allowances;
 
     // Events
 
-    // Transfer event
-    // MUST trigger when tokens are transferred, including zero value transfers.
-    // A token contract which creates new tokens SHOULD trigger a Transfer event
-    // with the _from address set to 0x0 when tokens are created.
+    /*
+    Transfer event
+    MUST trigger when tokens are transferred, including zero value transfers.
+    A token contract which creates new tokens SHOULD trigger a Transfer event
+    with the _from address set to 0x0 when tokens are created.
+    */
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
+    /*
+    Approval event
+    MUST trigger on any successful call to approve(address _spender, uint256 _value).
+    */
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
-
-    // Constructor of the contract
-    constructor()
-    {
+    /*
+    Contract constructor
+    */
+    constructor() {
         _name = "AlphaCoin42";
         _symbol = "AC42";
         _totalSupply = 1_000_000 * 10 ** uint256(_decimals);
@@ -47,97 +56,127 @@ contract AlphaCoin42 {
 
         // Trigger the Transfer event
         emit Transfer(address(0), msg.sender, _totalSupply);
-
     }
 
-    // Returns the name of the token - e.g. "AlphaCoin42".
-    function name() public view returns (string memory)
-    {
+    /*
+    Returns the name of the token - e.g. "AlphaCoin42".
+    */
+    function name() public view returns (string memory) {
         return _name;
     }
 
-    // Returns the symbol of the token. e.g. "AC42".
-    function symbol() public view returns (string memory)
-    {
+    /*
+    Returns the symbol of the token. e.g. "AC42".
+    */
+    function symbol() public view returns (string memory) {
         return _symbol;
     }
 
-    // Returns the number of decimals the token uses.
-    // For example, suppose I'm creating a token called "dollars,"
-    // and I want people to be able to transfer an amount like $1.25.
-    // To support that, I'll use a decimals value of 2.
-    function decimals() public view returns (uint8)
-    {
+    /*
+    Returns the number of decimals the token uses.
+    For example, suppose I'm creating a token called "dollars,"
+    and I want people to be able to transfer an amount like $1.25.
+    To support that, I'll use a decimals value of 2.
+    */
+    function decimals() public view returns (uint8) {
         return _decimals;
     }
 
-    // Returns the total token supply.
-    // The total token supply is the number of tokens available to the public.
-    function totalSupply() public view returns (uint256)
-    {
+    /*
+    Returns the total token supply.
+    The total token supply is the number of tokens available to the public.
+    */
+    function totalSupply() public view returns (uint256) {
         return _totalSupply;
     }
 
-
-    // Mapping from token owner address to their balance.
-    mapping(address => uint256) private _balances;
-
-    // balanceOf function returns the balance of the given address.
-    function balanceOf(address owner_) public view returns (uint256 balance)
-    {
+    /*
+    balanceOf function returns the balance of the given address.
+    */
+    function balanceOf(address owner_) public view returns (uint256) {
         return _balances[owner_];
     }
 
-
-    // Transfer function transfers tokens from the sender's address to the recipient's address.
-    function transfer(address to_, uint256 value_) public returns (bool success)
-    {
+    /*
+    Transfer function transfers tokens from the sender's address to the recipient's address.
+    */
+    function transfer(address to_, uint256 value_) public returns (bool success) {
         require(to_ != address(0), "ERC20: transfer to the zero address");
-        require(value_ <= totalSupply(), "ERC20: transfer amount exceeds total supply");
         require(_balances[msg.sender] >= value_, "ERC20: transfer amount exceeds balance");
-
         _balances[msg.sender] -= value_;
         _balances[to_] += value_;
-
         emit Transfer(msg.sender, to_, value_);
-
         return true;
     }
 
-    /**
-     * transferFrom:
-     *
-     * Transfers _value amount of tokens from address _from to address _to, and MUST fire the Transfer event.
-     *
-     * The transferFrom method is used for a withdraw workflow, allowing
-     * contracts to transfer tokens on your behalf. This can be used for
-     * example to allow a contract to transfer tokens on your behalf and/or to
-     * charge fees in sub-currencies.
-     * The function SHOULD throw unless the _from account has deliberately
-     * authorized the sender of the message via some mechanism.
-     *
-     * Note Transfers of 0 values MUST be treated as normal transfers and fire the Transfer event.
-     */
-    function transferFrom(address from_, address to_, uint256 value_) public returns (bool success)
-    {
-        // require(from_ != address(0), "ERC20: transfer from the zero address");
-        // require(to_ != address(0), "ERC20: transfer to the zero address");
-        // require(value_ <= totalSupply(), "ERC20: transfer amount exceeds total supply");
-        // require(_balances[from_] >= value_, "ERC20: transfer amount exceeds balance");
-
-        // require(from_ == msg.sender, "ERC20: transferFrom caller is not the owner");
-
-        // _balances[from_] -= value_;
-        // _balances[to_] += value_;
-
-        // emit Transfer(from_, to_, value_);
-
-        // return true;
+    /*
+    allowance
+    Returns the amount which spender_ is still allowed to withdraw from owner_.
+    */
+    function allowance(address owner_, address spender_) public view returns (uint256) {
+        return _allowances[owner_][spender_];
     }
 
+    /*
+    approve
+    Allows _spender to withdraw from your account multiple times, up to the
+    _value amount. If this function is called again it overwrites the
+    current allowance with _value.
+    NOTE: To prevent attack vectors like the one described here and discussed
+    here, clients SHOULD make sure to create user interfaces in such a way
+    that they set the allowance first to 0 before setting it to another value
+    for the same spender. THOUGH The contract itself shouldn’t enforce it,
+    to allow backwards compatibility with contracts deployed before
+    */
+    function approve(address spender_, uint256 value_) public returns (bool success) {
+        require(spender_ != address(0), "ERC20: approve to the zero address");
+        require(_allowances[msg.sender][spender_] == 0 || value_ == 0,
+                "ERC20: must first set allowance to zero");
+        _allowances[msg.sender][spender_] = value_;
+        emit Approval(msg.sender, spender_, value_);
+        return true;
+    }
 
-    // approve
+    /*
+    Pour eviter deux calls à approve, on peut utiliser les fonctions
+    increaseAllowance et decreaseAllowance
+    */
+    function increaseAllowance(address spender_, uint256 addedValue) public returns (bool) {
+        _allowances[msg.sender][spender_] += addedValue;
+        emit Approval(msg.sender, spender_, _allowances[msg.sender][spender_]);
+        return true;
+    }
 
-    // allowance
+    function decreaseAllowance(address spender_, uint256 subtractedValue) public returns (bool) {
+        require(_allowances[msg.sender][spender_] >= subtractedValue, "ERC20: decreased allowance below zero");
+        _allowances[msg.sender][spender_] -= subtractedValue;
+        emit Approval(msg.sender, spender_, _allowances[msg.sender][spender_]);
+        return true;
+    }
+
+    /*
+    transferFrom:
+    Transfers _value amount of tokens from address _from to address _to, and MUST fire the Transfer event.
+    The transferFrom method is used for a withdraw workflow, allowing
+    contracts to transfer tokens on your behalf. This can be used for
+    example to allow a contract to transfer tokens on your behalf and/or to
+    charge fees in sub-currencies.
+    The function SHOULD throw unless the _from account has deliberately
+    authorized the sender of the message via some mechanism.
+    Note Transfers of 0 values MUST be treated as normal transfers and fire the Transfer event.
+    */
+    function transferFrom(address from_, address to_, uint256 value_) public returns (bool success) {
+        require(value_ <= _allowances[from_][msg.sender], "ERC20: insufficient allowance");
+        require(value_ <= _balances[from_], "ERC20: transfer amount exceeds balance");
+        require(to_ != address(0), "ERC20: transfer to the zero address");
+
+        _balances[from_] -= value_;
+        _balances[to_] += value_;
+        _allowances[from_][msg.sender] -= value_;
+
+        emit Transfer(from_, to_, value_);
+
+        return true;
+    }
 
 }
--- code/test/AlphaCoin42.js
+++ code/test/AlphaCoin42.js
@@ -95,23 +95,17 @@ describe("AlphaCoin42 contract", function () {
         it("Should emit a Transfer event", async function () {
             const { hardhatToken, owner, addr1 } = await loadFixture(deployTokenFixture);
             const amount = BigInt(100);
-            await hardhatToken.transfer(addr1.address, amount);
-            const transferEvents = await hardhatToken.queryFilter("Transfer");
-            expect(transferEvents.length).to.equal(2);  // 1 for the deployment, 1 for the transfer
-            const event = transferEvents[1];
-            expect(event.args._from).to.equal(owner.address);
-            expect(event.args._to).to.equal(addr1.address);
-            expect(event.args._value).to.equal(amount);
+            await expect(hardhatToken.transfer(addr1.address, amount))
+                .to.emit(hardhatToken, "Transfer")
+                .withArgs(owner.address, addr1.address, amount);
         });
 
         it("Should fail if the sender does not have enough tokens", async function () {
             const { hardhatToken, owner, addr1 } = await loadFixture(deployTokenFixture);
-            const amount = BigInt(1_000_000) * BigInt(10) ** BigInt(18);
-            hardhatToken.transfer(addr1.address, amount);
-            await expect(hardhatToken.transfer(addr1.address, 1)).to.be.revertedWith(
+            const amount = BigInt(1_000_000) * BigInt(10) ** BigInt(18) + BigInt(1);
+            await expect(hardhatToken.transfer(addr1.address, amount)).to.be.revertedWith(
                 "ERC20: transfer amount exceeds balance"
             );
-
         });
 
         it("Should fail if the recipient is the zero address", async function () {
@@ -125,23 +119,162 @@ describe("AlphaCoin42 contract", function () {
         it("Transfers of 0 values MUST be treated as normal transfers and fire the Transfer event", async function () {
             const { hardhatToken, owner, addr1 } = await loadFixture(deployTokenFixture);
             const amount = BigInt(0);
-            await hardhatToken.transfer(addr1.address, amount);
-            const transferEvents = await hardhatToken.queryFilter("Transfer");
-            expect(transferEvents.length).to.equal(2);  // 1 for the deployment, 1 for the transfer
-            const event = transferEvents[1];
-            expect(event.args._from).to.equal(owner.address);
-            expect(event.args._to).to.equal(addr1.address);
-            expect(event.args._value).to.equal(amount);
-        });
-
-        it("Should not be possible to transfer more than the total supply", async function () {
-            const { hardhatToken, owner, addr1 } = await loadFixture(deployTokenFixture);
-            const totalSupply = await hardhatToken.totalSupply();
-            await expect(hardhatToken.transfer(addr1.address, totalSupply + BigInt(1))).to.be.revertedWith(
-                "ERC20: transfer amount exceeds total supply"
-            );
+            await expect(hardhatToken.transfer(addr1.address, amount))
+                .to.emit(hardhatToken, "Transfer")
+                .withArgs(owner.address, addr1.address, amount);
         });
 
     });
+
+    describe("allowance", function () {
+
+        it("Should return the allowance of the owner", async function () {
+            const { hardhatToken, owner, addr1 } = await loadFixture(deployTokenFixture);
+            const amount = BigInt(100);
+            await hardhatToken.approve(addr1.address, amount);
+            const allowance = await hardhatToken.allowance(owner.address, addr1.address);
+            expect(allowance).to.equal(amount);
+        });
+
+        it("Should return 0 for an address with no allowance", async function () {
+            const { hardhatToken, owner, addr1, addr2 } = await loadFixture(deployTokenFixture);
+            const amount = BigInt(100);
+            await hardhatToken.approve(addr1.address, amount);
+            const allowance = await hardhatToken.allowance(owner.address, addr2.address);
+            expect(allowance).to.equal(0);
+        });
+
+        it("Should allow setting allowance", async function () {
+            const { hardhatToken, owner, addr1 } = await loadFixture(deployTokenFixture);
+            const amount = BigInt(500);
+            await expect(hardhatToken.approve(addr1.address, amount))
+                .to.emit(hardhatToken, "Approval")
+                .withArgs(owner.address, addr1.address, amount);
+            expect(await hardhatToken.allowance(owner.address, addr1.address)).to.equal(amount);
+        });
+
+        it("Should emit an Approval event", async function () {
+            const { hardhatToken, owner, addr1 } = await loadFixture(deployTokenFixture);
+            const amount = BigInt(100);
+            await expect(hardhatToken.approve(addr1.address, amount))
+                .to.emit(hardhatToken, "Approval")
+                .withArgs(owner.address, addr1.address, amount);
+        });
+
+        it("Should fail if the spender is the zero address", async function () {
+            const { hardhatToken, owner } = await loadFixture(deployTokenFixture);
+            const amount = BigInt(100);
+            await expect(hardhatToken.approve("0x0000000000000000000000000000000000000000", amount)).to.be.revertedWith(
+                "ERC20: approve to the zero address"
+            );
+        });
+
+        it("Should allow spending within the approved allowance", async function () {
+            const { hardhatToken, owner, addr1, addr2 } = await loadFixture(deployTokenFixture);
+            const amount = BigInt(200);
+            await hardhatToken.approve(addr1.address, amount);
+
+            await hardhatToken.connect(addr1).transferFrom(owner.address, addr2.address, amount);
+
+            expect(await hardhatToken.balanceOf(addr2.address)).to.equal(amount);
+            expect(await hardhatToken.allowance(owner.address, addr1.address)).to.equal(0);
+        });
+
+        it("Should fail if spender tries to spend more than allowed", async function () {
+            const { hardhatToken, owner, addr1, addr2 } = await loadFixture(deployTokenFixture);
+            const amount = BigInt(100);
+            await hardhatToken.approve(addr1.address, amount);
+
+            await expect(
+                hardhatToken.connect(addr1).transferFrom(owner.address, addr2.address, amount + BigInt(1))
+            ).to.be.revertedWith("ERC20: insufficient allowance");
+        });
+
+    });
+
+    describe("Increase and Decrease Allowance", function () {
+
+        it("Should increase the allowance", async function () {
+            const { hardhatToken, owner, addr1 } = await loadFixture(deployTokenFixture);
+            const initialAmount = BigInt(200);
+            await hardhatToken.approve(addr1.address, initialAmount);
+
+            const increaseAmount = BigInt(100);
+            await hardhatToken.increaseAllowance(addr1.address, increaseAmount);
+
+            expect(await hardhatToken.allowance(owner.address, addr1.address)).to.equal(initialAmount + increaseAmount);
+        });
+
+        it("Should emit an Approval event on increaseAllowance", async function () {
+            const { hardhatToken, owner, addr1 } = await loadFixture(deployTokenFixture);
+            const increaseAmount = BigInt(50);
+            await expect(hardhatToken.increaseAllowance(addr1.address, increaseAmount))
+                .to.emit(hardhatToken, "Approval")
+                .withArgs(owner.address, addr1.address, increaseAmount);
+        });
+
+        it("Should decrease the allowance", async function () {
+            const { hardhatToken, owner, addr1 } = await loadFixture(deployTokenFixture);
+            const initialAmount = BigInt(300);
+            await hardhatToken.approve(addr1.address, initialAmount);
+
+            const decreaseAmount = BigInt(100);
+            await hardhatToken.decreaseAllowance(addr1.address, decreaseAmount);
+
+            expect(await hardhatToken.allowance(owner.address, addr1.address)).to.equal(initialAmount - decreaseAmount);
+        });
+
+        it("Should fail if trying to decrease allowance below zero", async function () {
+            const { hardhatToken, owner, addr1 } = await loadFixture(deployTokenFixture);
+            const initialAmount = BigInt(100);
+            await hardhatToken.approve(addr1.address, initialAmount);
+
+            await expect(
+                hardhatToken.decreaseAllowance(addr1.address, initialAmount + BigInt(1))
+            ).to.be.revertedWith("ERC20: decreased allowance below zero");
+        });
+
+    });
+
+    describe("transferFrom", function () {
+
+        it("Should allow transferFrom when approved", async function () {
+            const { hardhatToken, owner, addr1, addr2 } = await loadFixture(deployTokenFixture);
+            await hardhatToken.approve(addr1.address, 200);
+            await hardhatToken.connect(addr1).transferFrom(owner.address, addr2.address, 100);
+            expect(await hardhatToken.balanceOf(addr2.address)).to.equal(100);
+        });
+
+        it("Should decrease allowance on transferFrom", async function () {
+            const { hardhatToken, owner, addr1, addr2 } = await loadFixture(deployTokenFixture);
+            await hardhatToken.approve(addr1.address, 200);
+            await hardhatToken.connect(addr1).transferFrom(owner.address, addr2.address, 100);
+            expect(await hardhatToken.allowance(owner.address, addr1.address)).to.equal(100);
+        });
+
+        it("Should not allow transferFrom more than allowed", async function () {
+            const { hardhatToken, owner, addr1, addr2 } = await loadFixture(deployTokenFixture);
+            await hardhatToken.approve(addr1.address, 100);
+            await expect(hardhatToken.connect(addr1).transferFrom(owner.address, addr2.address, 200))
+                .to.be.revertedWith("ERC20: insufficient allowance");
+        });
+    });
+
+    describe("Edge Cases", function () {
+        it("Should allow transferring to self", async function () {
+            const { hardhatToken, owner } = await loadFixture(deployTokenFixture);
+            await hardhatToken.transfer(owner.address, 100);
+            expect(await hardhatToken.balanceOf(owner.address)).to.be.gt(0);
+        });
+
+        it("Should not allow exceeding MaxUint256 in increaseAllowance", async function () {
+            const { hardhatToken, owner, addr1 } = await loadFixture(deployTokenFixture);
+            await hardhatToken.approve(addr1.address, MaxUint256);
+            await expect(hardhatToken.increaseAllowance(addr1.address, 1))
+                .to.be.reverted;
+        });
+
+    });
+
 
 });
