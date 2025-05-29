29e8ef93b832ad3eecfe315ec0cb22e84c29e6bc
Charles <contact@charles-mariot.fr>
2025-05-29 17:29:29 +0200

    Fix deployment module constructor

--- bonus/ignition/modules/Niel42MultiSig.ts
+++ bonus/ignition/modules/Niel42MultiSig.ts
@@ -1,6 +1,7 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("Niel42MultiSigModule", (m) => {
-  const contract = m.contract("Niel42MultiSig");
+  const deployer = m.getAccount(0);
+  const contract = m.contract("Niel42MultiSig", [[deployer], 1]);
   return { contract };
 });
